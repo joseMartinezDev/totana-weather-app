9335c24251d157c8d10cbae07a1269b2c34042db
Jose Martinez <jmartinez@and.digital>
2020-07-03 23:44:22 +0100

    Initial commit

---
+++ app/src/main/java/com/example/totana_weather_app/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.totana_weather_app.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.totana_weather_app.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
